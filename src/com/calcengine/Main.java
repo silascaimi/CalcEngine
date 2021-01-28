bd8fe9d8f65519516e595062481b56196a81c53a
Silas dos Santos Caimi <silas.caimi@cnpq.br>
2021-01-28 19:33:50 -0300

    add files

---
+++ src/com/calcengine/Main.java
@@ -0,0 +1,22 @@
+package com.calcengine;
+
+public class Main {
+
+	public static void main(String[] args) {
+		performCalculations();
+	}
+
+	private static void performCalculations() {
+
+		MathEquation[] equations = new MathEquation[4];
+		equations[0] = new MathEquation(100.0d, 50.0d, 'd');
+		equations[1] = new MathEquation(25.0d, 92.0d, 'a');
+		equations[2] = new MathEquation(225.0d, 17.0d, 's');
+		equations[3] = new MathEquation(11.0d, 3.0d, 'm');
+
+		for (MathEquation equation : equations) {
+			equation.execute();
+			System.out.println("result = " + equation.getResult());
+		}
+	}
+}
